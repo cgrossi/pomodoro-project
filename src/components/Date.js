e8cbc3377f6af9be29375c17b9637a3529ce4cc6
cgrossi <cgrossi@gmail.com>
2019-08-29 11:24:41 -0600

    Set up base structure, css, favicon

---
+++ src/components/Date.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Date extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Date
